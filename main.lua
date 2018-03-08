e3b4f33e19d3b9a9c262ab3c0d48ffadfd231a07
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-03-08 10:24:12 -0500

    yes

--- main.lua
+++ main.lua
@@ -8,8 +8,8 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 --global variables
-scrollSpeed = 3
-scrollSpeed2 = 6
+scrollSpeed = 3 --Obama
+scrollSpeed2 = 6 
 scrollSpeed3 = 2
 
 --background image with width and height
@@ -39,29 +39,3 @@ obama.y = display.contentHeight*2/3
 potato.x = 50
 potato.y = display.contentHeight*3/3
 
---Function: MoveObama
---Input: this function accepts an event listener
---Output: none
---Description: This function adds the scroll speed to the x-value of obama
-local function MoveObama(event)
-	--add the scroll speed to the x-value of obama
-	obama.x = obama.x + scrollSpeed
-
-	-- change the transparency of obama every time he moves so that he fades in
-	obama.alpha = obama.alpha - 0.1
-
-end
-
--- Move will be called over and over again
-Runtime:addEventListener("enterFrame", MoveObama)
-
-local function MoveObama(event)
-	--add the scroll speed to the x-value of obama
-	obama.x = obama.x + scrollSpeed
-
-	-- change the transparency of obama every time he moves so that he fades in
-	obama.alpha = obama.alpha - 0.1
-
-	--made obama grow
-	obama:scale(1 + 0.005, 1 + 0.005)
-end
